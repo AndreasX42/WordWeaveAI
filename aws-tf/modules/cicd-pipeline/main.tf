1322a4563a75200acc0d75554934409e9b4b81f0
AndreasX42 <andreas.pangerl.42@gmail.com>
2025-07-27 11:25:23 -0400

    fix cb arch

--- aws-tf/modules/cicd-pipeline/main.tf
+++ aws-tf/modules/cicd-pipeline/main.tf
@@ -250,8 +250,8 @@ resource "aws_codebuild_project" "frontend" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                      = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type                       = "LINUX_CONTAINER"
+    image                      = "aws/codebuild/amazonlinux-aarch64-standard:3.0"
+    type                       = "ARM_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode            = true
 
@@ -322,8 +322,8 @@ resource "aws_codebuild_project" "backend" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                      = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type                       = "LINUX_CONTAINER"
+    image                      = "aws/codebuild/amazonlinux-aarch64-standard:3.0"
+    type                       = "ARM_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode            = true
 
